5a44693122cea5c4d988dba88ab00b833de0c9a7
shubhamgiri729 <shubhamgiri729@gmail.com>
2026-04-02 18:09:30 +0530

    implemented right, inverted and left triangle star patterns

--- star_pattern.py
+++ star_pattern.py
@@ -1,13 +1,43 @@
 '''
-Print:
+Print: Right Triangle
 
 *
 **
 ***
 ****
+*****
 '''
-
-n=4
+print("Right Angle Star Pattern")
+n=5
 for i in range(1,n+1):
     print("*"*i)
     
+'''
+print:Inverted Triangle
+
+*****
+****
+***
+**
+*
+'''
+
+print("Inverted Star Pattern")
+n=5
+for i in range(1,n+1):
+    print("*"*((n+1)-i))
+    
+
+'''
+print:Left Triangle
+    *
+   **
+  ***
+ ****
+*****
+'''
+
+print("Left Angle Star Pattern")
+n=5
+for i in range(1,n+1):
+    print(" "*(n-i) + "*"*i)
