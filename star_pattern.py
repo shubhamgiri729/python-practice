e2362b3564122a841404731df2cfb6fd3fbec488
shubhamgiri729 <shubhamgiri729@gmail.com>
2026-04-02 19:00:18 +0530

    implemented inverted left,pyramid and inverted Pyramid star patterns

--- star_pattern.py
+++ star_pattern.py
@@ -13,7 +13,7 @@ for i in range(1,n+1):
     print("*"*i)
     
 '''
-print:Inverted Triangle
+print:Inverted Right Angle Triangle
 
 *****
 ****
@@ -22,7 +22,7 @@ print:Inverted Triangle
 *
 '''
 
-print("Inverted Star Pattern")
+print("Inverted Right Angle Star Pattern")
 n=5
 for i in range(1,n+1):
     print("*"*((n+1)-i))
@@ -41,3 +41,44 @@ print("Left Angle Star Pattern")
 n=5
 for i in range(1,n+1):
     print(" "*(n-i) + "*"*i)
+    
+'''
+print:Inverted Left Angle Pattern
+
+*****
+ ****
+  ***
+   **
+    *
+'''
+print("Inverted Left Angle star Pattern")
+n=5
+for i in range(n):
+    print(" "*i+"*"*(n-i))
+    
+'''
+print:Pyramid (Centered)
+    *
+   ***
+  *****
+ *******
+*********
+'''
+print("Pyramid Pattern")
+n=5
+for i in  range(1,n+1):
+    print(" "*(n-i)+"*"*((2*i)-1))
+    
+    
+'''
+print:Inverted Pyramid
+*********
+ *******
+  *****
+   ***
+    *
+'''
+print("Inverted Pyramid Pattern")
+n=5
+for i in range(n):
+    print(" "*i+"*"*(2*(n-i)-1))
