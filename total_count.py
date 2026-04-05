57fff6cc3edc04a045e680dc3db3f091d9bf5ae7
shubhamgiri729 <shubhamgiri729@gmail.com>
2026-04-05 23:44:22 +0530

    added program to count consonants

--- total_count.py
+++ total_count.py
@@ -1,9 +1,17 @@
 #Count Total Number of Vowels in the String
 
 str="Encyclopedia"
-count=0
+vowel_count=0
 for ch in str:
     if ch in 'aeiouAEIOU':
-        count+=1
-print("Number of Vowels:",count)
+        vowel_count+=1
+print("Number of Vowels:",vowel_count)
 
+#Count Total Number of Consonants in the String
+
+text="Encyclopedia"
+consonant_count=0
+for ch in text:
+    if ch not in 'aeiouAEIOU':
+        consonant_count+=1
+print("Number of Consonants:",consonant_count)
