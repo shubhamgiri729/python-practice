0e028e2e23025b266174ce481399de752b80cfb5
shubhamgiri729 <shubhamgiri729@gmail.com>
2026-04-08 22:19:39 +0530

    added program to print prime numbers from 1 to n

--- prime.py
+++ prime.py
@@ -11,3 +11,16 @@ else:
             break
     else:
         print("Prime")
+        
+# Print prime numbers from 1 to n
+n = 20
+for num in range(2, n + 1):
+    is_prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(num, end=" ")
