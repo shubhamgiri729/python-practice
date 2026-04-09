f1e324e2c550f8fe2800699ec1250ee6299a3a6b
shubhamgiri729 <shubhamgiri729@gmail.com>
2026-04-09 22:33:14 +0530

    implemented lowercase, uppercase and capitalize string operations

---
+++ string_case_conversion.py
@@ -0,0 +1,8 @@
+#Convert a string into: Lowercase / Uppercase / Capitalized format
+
+str="Hello world"
+
+print("Original:",str)
+print("Lowercase:",str.lower())
+print("Uppercase:",str.upper())
+print("Capitalize:",str.capitalize())
